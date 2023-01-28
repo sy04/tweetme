35649758eb9e44d5e5722a06a9c9fe1177fa8a2f
Syaiful Fitrawan <63946900+sy04@users.noreply.github.com>
2023-01-28 15:28:14 +0700

    🎨 indent spacing code change

--- tweets/migrations/0001_initial.py
+++ tweets/migrations/0001_initial.py
@@ -5,18 +5,18 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
+  initial = True
 
-    dependencies = [
-    ]
+  dependencies = [
+  ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Tweet',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content', models.TextField(blank=True, null=True)),
-                ('image', models.FileField(blank=True, null=True, upload_to='images/')),
-            ],
-        ),
-    ]
+  operations = [
+    migrations.CreateModel(
+      name='Tweet',
+      fields=[
+        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('content', models.TextField(blank=True, null=True)),
+        ('image', models.FileField(blank=True, null=True, upload_to='images/')),
+      ],
+    ),
+  ]
--- tweets/views.py
+++ tweets/views.py
@@ -5,32 +5,32 @@ from django.shortcuts import render
 from .models import Tweet
 # Create your views here.
 def home_page(request, *args, **kwargs):
-    return render(request, "pages/home.html", context={}, status=200)
+  return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_list_view(request, *args, **kwargs):
-    qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 500)} for x in qs]
-    data = {
-        "isUser": False,
-        "response": tweet_list
-    }
+  qs = Tweet.objects.all()
+  tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 500)} for x in qs]
+  data = {
+    "isUser": False,
+    "response": tweet_list
+  }
 
-    return JsonResponse(data)
+  return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    data = {
-        "id": tweet_id,
-        # "content": obj.content,
-        # "image_path": obj.image.url
-    }
+  data = {
+    "id": tweet_id,
+    # "content": obj.content,
+    # "image_path": obj.image.url
+  }
 
-    status = 200
+  status = 200
 
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = "Not Found"
-        status = 404
+  try:
+    obj = Tweet.objects.get(id=tweet_id)
+    data['content'] = obj.content
+  except:
+    data['message'] = "Not Found"
+    status = 404
 
-    return JsonResponse(data, status=status)
+  return JsonResponse(data, status=status)
