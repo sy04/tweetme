a8c32e26cbd91090965aef7b0486fb1e5d5ecc8d
Syaiful Fitrawan <63946900+sy04@users.noreply.github.com>
2023-01-28 12:57:50 +0700

    ⚡️ feat(URL): set url and try catch

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from tweets.views import home_page, tweet_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
+def home_page(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+        # "content": obj.content,
+        # "image_path": obj.image.url
+    }
+
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
