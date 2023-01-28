420b71c50c43a82c1ad0e0864869e16cd79dc8ca
Syaiful Fitrawan <63946900+sy04@users.noreply.github.com>
2023-01-29 00:49:42 +0700

    ✨ feat(Tweet): create tweet form

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
-from tweets.views import home_page, tweet_detail_view, tweet_list_view
+from tweets.views import (
+    home_page,
+    tweet_detail_view,
+    tweet_list_view,
+    tweet_create_view
+  )
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', home_page),
+  path('create-tweet', tweet_create_view),
   path('tweets', tweet_list_view),
   path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -3,9 +3,20 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
+from .forms import TweetForm
+
 # Create your views here.
 def home_page(request, *args, **kwargs):
   return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+  form = TweetForm(request.POST or None)
+  if form.is_valid():
+    obj = form.save(commit=False)
+    obj.save()
+    form = TweetForm()
+
+  return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
   qs = Tweet.objects.all()
