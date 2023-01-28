4ca4617d1d0ea56cf7dbb73ac104d1d19418d85c
Syaiful Fitrawan <63946900+sy04@users.noreply.github.com>
2023-01-29 01:36:12 +0700

    ⚡️ feat(Tweet): create new form and redirect

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,6 @@
 import random
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Tweet
 from .forms import TweetForm
@@ -11,8 +11,12 @@ def home_page(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
   form = TweetForm(request.POST or None)
+  next_url = request.POST.get("next") or None
+
   if form.is_valid():
     obj = form.save(commit=False)
+    if next_url != None:
+      return redirect(next_url)
     obj.save()
     form = TweetForm()
 
