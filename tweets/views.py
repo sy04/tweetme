04fc2c54faeccd5f37d34857956139557ac6e2ec
Syaiful Fitrawan <63946900+sy04@users.noreply.github.com>
2023-01-29 23:15:57 +0700

    🚑 feat(Tweet): add new validation

--- tweets/views.py
+++ tweets/views.py
@@ -30,6 +30,10 @@ def tweet_create_view(request, *args, **kwargs):
 
     form = TweetForm()
 
+  if form.errors:
+    if is_ajax:
+      return JsonResponse(form.errors, status=400)
+
   return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
